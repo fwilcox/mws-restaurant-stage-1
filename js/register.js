7bcbe63d9e3c6a06d0cdb5266153257c3272821d
fwilcox <fewilcox3@gmail.com>
2018-06-26 11:21:26 -0400

    feat: Added service worker to allow for offline functionality

---
+++ js/register.js
@@ -0,0 +1,13 @@
+/* Setup the SW  */
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load',function() {
+    navigator.serviceWorker.register('/sw.js')
+      .then(function(registration) {
+      // Check if successful
+      console.log('SW registration successsful with scope: ', registration.scope);
+    }, function(err) {
+      // It failed
+      console.log('SW registration failed: ', err);
+    });
+  });
+}
