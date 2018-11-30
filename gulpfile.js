17fbc67629607f6e40ccdbb9bcf81a4017998f5d
fwilcox <fewilcox3@gmail.com>
2018-11-29 21:10:45 -0500

    feat: Updated gulpfile and removed unuses IDB refrence

--- gulpfile.js
+++ gulpfile.js
@@ -47,19 +47,6 @@ gulp.task('sw', () => {
     .pipe(gulp.dest('.tmp/'));
 });
 
-gulp.task('helper', () => {
-  const bundleStream = browserify(
-    {debug: true}
-  );
-
-  return bundleStream
-    .transform(babelify)
-    .require('app/js/dbhelper.js'  , {entry:true})
-    .bundle()
-    .pipe(source('dbhelper.js'))
-    .pipe(gulp.dest('.tmp/js/'));
-});
-
 function lint(files) {
   return gulp.src(files)
     .pipe($.eslint({ fix: false }))
@@ -119,7 +106,7 @@ gulp.task('extras', () => {
 gulp.task('clean', del.bind(null, ['.tmp', 'dist']));
 
 gulp.task('serve', () => {
-  runSequence(['clean', 'wiredep'], ['styles', 'scripts', 'helper', 'sw', 'fonts'], () => {
+  runSequence(['clean', 'wiredep'], ['styles', 'scripts', 'sw', 'fonts'], () => {
     browserSync.init({
       notify: false,
       port: 8000,
@@ -138,8 +125,8 @@ gulp.task('serve', () => {
     ]).on('change', reload);
 
     gulp.watch('app/css/**/*.css', ['styles']);
-    gulp.watch('app/js/**/*.js', ['scripts', 'helper']);
-    gulp.watch('app/sw.js');
+    gulp.watch('app/js/**/*.js', ['scripts']);
+    gulp.watch('app/sw.js')['sw'];
     gulp.watch('app/fonts/**/*', ['fonts']);
     gulp.watch('bower.json', ['wiredep', 'fonts']);
   });
